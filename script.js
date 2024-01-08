643730a2229c173fad11d72a01a81e74848bfea1
Akindele <faroukakindele0@gmail.com>
2024-01-08 02:46:58 -0500

    initiated email incoporation

--- script.js
+++ script.js
@@ -1,103 +1,111 @@
-//  variable decleration 
-const InputName = document.querySelector('#cardHolder')
-const cardName = document.querySelector(".name")
-const cardNumber = document.querySelector('.number')
-const InputNumber = document.querySelector("#cardNumber")
-const inputMonth = document.querySelector('#cardYear')
-const cardMonth = document.querySelector(".month")
-const inputYear = document.querySelector('#cardYear')
-const cardYear = document.querySelector(".year")
-const cardCvc = document.querySelector('.cvv')
-const inputCvc = document.querySelector("#cvc")
-const confirmBtn = document.querySelector(".confirm")
-const form = document.querySelector(".form")
-const success = document.querySelector(".success")
-const wrongFormat = document.querySelectorAll(".wrongFormat")
-let checkbox
-let nameStr
+//  variable decleration
+const InputName = document.querySelector("#cardHolder");
+const cardName = document.querySelector(".name");
+const cardNumber = document.querySelector(".number");
+const InputNumber = document.querySelector("#cardNumber");
+const inputMonth = document.querySelector("#cardMonth");
+const cardMonth = document.querySelector(".month");
+const inputYear = document.querySelector("#cardYear");
+const cardYear = document.querySelector(".year");
+const cardCvc = document.querySelector(".cvv");
+const inputCvc = document.querySelector("#cvc");
+const confirmBtn = document.querySelector(".confirm");
+const form = document.querySelector(".form");
+const success = document.querySelector(".success");
+const wrongFormat = document.querySelectorAll(".wrongFormat");
+const email = document.querySelector("#cardEmail");
+let checkbox;
+let nameStr;
 
 // capitalise Name
 function capitaliseWords(str) {
-    if (str) {
-        nameStr = str
-        let nameOne = str.split(" ")[0]
-        nameOne = nameOne[0].toUpperCase() + nameOne.slice(1)
-        let nameTwo = str.split(" ")[1]
-        if (nameTwo) nameTwo = nameTwo[0].toUpperCase() + nameTwo.slice(1)
-        else return nameTwo = " "
-        const realName = nameOne + " " + nameTwo
-        return realName
-    }
+  if (str) {
+    nameStr = str;
+    let nameOne = str.split(" ")[0];
+    nameOne = nameOne[0].toUpperCase() + nameOne.slice(1);
+    let nameTwo = str.split(" ")[1];
+    if (nameTwo) nameTwo = nameTwo[0].toUpperCase() + nameTwo.slice(1);
+    else return (nameTwo = " ");
+    const realName = nameOne + " " + nameTwo;
+    return realName;
+  }
 }
 
 // show error
 function showError(n) {
-    wrongFormat[n].style.visibility = "visible"
-
+  wrongFormat[n].style.visibility = "visible";
 }
 // Format card input
 function formatCardNumber(number) {
-    checkbox = (parseInt(number) + "").length
-    if (!number) return
-    let cardNo = number.match(/.{1,4}/g).join(" ")
-    return cardNo
+  checkbox = (parseInt(number) + "").length;
+  if (!number) return;
+  let cardNo = number.match(/.{1,4}/g).join(" ");
+  return cardNo;
 }
-// show month errir
+// show month error
 function checkMonthYear() {
-    if (!inputYear.value || !inputMonth.value)
-        showError(2)
-    if (!inputCvc.value) showError(3)
-    if (!nameStr.trim().includes(" ")) showError(0)
-
+  if (!inputYear.value || !inputMonth.value) {
+    inputYear.style.borderColor = "red";
+    inputMonth.style.borderColor = "red";
+    showError(2);
+  }
+  if (!inputCvc.value) {
+    inputCvc.style.borderColor = "red";
+    showError(3);
+  }
+  if (!nameStr?.trim().includes(" ")) showError(0);
 }
 
 // display properties
 function displayProperties() {
-    cardName.textContent = capitaliseWords(InputName.value) || "Jane Applseed"
-    cardNumber.textContent = formatCardNumber(InputNumber.value) || "0000 0000 0000 0000"
-    cardYear.textContent = inputYear.value || "00"
-    cardMonth.textContent = inputMonth.value || '00'
-    cardCvc.textContent = inputCvc.value
-
+  cardName.textContent = capitaliseWords(InputName.value) || "Jane Applseed";
+  cardNumber.textContent =
+    formatCardNumber(InputNumber.value) || "0000 0000 0000 0000";
+  cardYear.textContent = inputYear.value || "00";
+  cardMonth.textContent = inputMonth.value || "00";
+  cardCvc.textContent = inputCvc.value;
 }
+// Restore value on REload
 function restore() {
-    InputName.style.borderColor = "grey"
-    wrongFormat.forEach(
-        x => x.style.visibility = "hidden"
-    )
-    wrongFormat[1].textContent = "pls check card number"
-
+  InputName.style.borderColor = "grey";
+  wrongFormat.forEach((x) => (x.style.visibility = "hidden"));
+  wrongFormat[1].textContent = "pls check card number";
 }
 
-confirmBtn.addEventListener(
-    "click", () => {
-        // restore Color
-        restore()
-        // Check For AlphaBets Errors
-        if (hasAlphabet(InputNumber.value)) [
-            wrongFormat[1].textContent = "Wrong Format Numbers Only"
-        ]
-        // Validate and Displays inputs data
-        displayProperties()
-        checkMonthYear()
+confirmBtn.addEventListener("click", () => {
+  // restore Color
+  restore();
+  // Check For AlphaBets Errors
+  if (hasAlphabet(InputNumber.value))
+    [(wrongFormat[1].textContent = "Wrong Format Numbers Only")];
+  // Validate and Displays inputs data
+  displayProperties();
+  checkMonthYear();
 
-        // Confirm Validation and Show all Error
-        if (checkbox < 16) {
-            showError(1)
-            InputNumber.style.borderColor = "red"
-        }
-        if (checkbox === 16 && InputName.value
-            && inputCvc.value && inputMonth.value && nameStr.trim().includes(" ")) {
-            form.style.display = "none"
-            success.style.display = "block"
-        }
-
-
-    }
-)
+  // Confirm Validation and Show all Error
+  if (checkbox < 16) {
+    showError(1);
+    InputNumber.style.borderColor = "red";
+  }
+  if (!InputName.value) {
+    InputName.style.borderColor = "red";
+  }
+  if (
+    checkbox === 16 &&
+    InputName.value &&
+    inputCvc.value &&
+    inputMonth.value &&
+    nameStr?.trim().includes(" ")
+  ) {
+    form.style.display = "none";
+    success.style.display = "block";
+  }
+});
 // show alphabetical error
 function hasAlphabet(str) {
-    let test = /[a-zA-Z]/
+  let test = /[a-zA-Z]/;
 
-    return test.test(str)
+  return test.test(str);
 }
+
+function sendEmail() {}
